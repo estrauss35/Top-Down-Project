0f1ce5a6b5dbaa417630a6202d1e0d3cb61ccab4
estrauss35 <ems394@nau.edu>
2016-10-31 12:13:49 -0700

    Revert "Revert "Player Movement Script""
    This reverts commit f382fa0312a80ab4c4c8d1622b9e621b8e69ae44.

---
+++ PlayerJump.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJump : MonoBehaviour {
+
+	public bool CanJump = true;
+	public float MaxJumpTime = 2f;
+	private float JumpTime = 0f;
+	// Use this for initialization
+	void Start () {
+		JumpTime = MaxJumpTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!CanJump) {
+			JumpTime -= Time.deltaTime;
+			if(JumpTime <= 0 && GetComponent<Rigidbody2D>().velocity.y == 0){
+				CanJump = true;
+				JumpTime = MaxJumpTime;
+			}
+		}
+
+	}
+
+	void FixedUpdate(){
+		Jump ();
+	}
+
+	void Jump(){
+		if (Input.GetKeyDown ("space") && CanJump) {
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0,6), ForceMode2D.Impulse);
+			CanJump = false;
+
+		}
+	}
+
+
+}
